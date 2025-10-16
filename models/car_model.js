4147d58aee939648dfeeb7335c5fd1d4a2daf774
Shriraj Lakeshri <Lakeshri1230@gmail.com>
2025-10-16 18:18:50 +0530

    Added Car model schema

---
+++ models/car_model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+    carID: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    make: {
+        type: String,
+        required: true,
+    },
+    model: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    }
+
+
+},{timestamps: true});
+
+module.export   = mongoose.model("Car", carSchema)
