a85f9fdc04ba63e0cbe339dc54a8230313fd3267
Shri <110012624+pshri5@users.noreply.github.com>
2025-10-21 15:53:28 +0530

    Update db file

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
 
 export const connectDB = async() =>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
-        console.log("MongoDB connected successfully!")
+        console.log(`MongoDB connected successfully! DB connection host: ${DB_NAME}`)
     } catch (error) {
         console.log("MongoDB connection error",error)
         process.exit(1)
