dc12c60a213378c53ab00ecb19f19d3db897a710
Shri <110012624+pshri5@users.noreply.github.com>
2025-10-21 15:42:17 +0530

    added dotenv and minor changes

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env",
+    credentials: true
+})
+
 
 
 const app = express()
+
+app.use(cors())
 
 
 
