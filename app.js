1d3b186b3626a244b9ef6490314b55242ed58327
Shri <110012624+pshri5@users.noreply.github.com>
2025-10-21 15:47:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ import dotenv from "dotenv"
 
 dotenv.config({
     path: "./.env",
-    credentials: true
 })
-
-
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.PORT,
+    credentials: true
+}))
+app.use(express.json({limit: "20kb"}))
+app.use(express.urlencoded({extended: true, limit:"20kb"}))
 
 
 
