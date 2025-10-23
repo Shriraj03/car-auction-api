9d6df455aa6f255f90b5e8b9c8e29d3b9e45284d
Preetam Shri <preetamsshri@gmail.com>
2025-10-23 19:37:03 +0530

    Update dealer model
    Added bcrypt password hashing and comparing methods

--- models/dealer.model.js
+++ models/dealer.model.js
@@ -1,13 +1,34 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const dealerSchema = new mongoose.Schema({
     dealerId : {type:String,required:true,unique:true},
     dealerName :{type:String,required:true},
     dealerEmail : {type:String,required:true,unique:true,lowercase:true},
+    dealerPassword : {type:String,required: true, unique: true},
     auctions :[{type:mongoose.Schema.Types.ObjectId,ref:"Auction"}],
     TimeStamps : {createdAt:true,updatedAt:false}
 });
 
+//Hashing the password before saving
+dealerSchema.pre("save",async function (next) {
+    if(!this.isModified("dealerPassword")) return next() // preventing rehashing saved password
+    
+    this.dealerPassword = await bcrypt.hash(this.dealerPassword,10)
+    next()
+})
+
+//Comparing the saved password and input password
+dealerSchema.methods.isPasswordCorrect = async function(dealerPassword){
+    return await bcrypt.compare(dealerPassword,this.dealerPassword)
+}
+
+
+
+
+
+
 const Dealer = mongoose.model("Dealer", dealerSchema);
 
 export default Dealer;
+
